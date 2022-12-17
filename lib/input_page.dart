8d7c2c1ed03fb3852634ab5d89f1e3973a0b9666
Júlia Cardozo <ccardozojulia@gmail.com>
2022-12-17 20:35:27 -0300

    input page

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -13,8 +13,47 @@ class _InputPageState extends State<InputPage> {
         centerTitle: true,
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(children: [
+        Expanded(
+          child: Row(children: [
+            Expanded(
+              child: ReusableCard(colour: Color(0xFF1D1E33)),
+            ),
+            Expanded(
+              child: ReusableCard(colour: Color(0xFF1D1E33)),
+            ),
+          ]),
+        ),
+        Expanded(
+          child: ReusableCard(colour: Color(0xFF1D1E33)),
+        ),
+        Expanded(
+          child: Row(children: [
+            Expanded(
+              child: ReusableCard(colour: Color(0xFF1D1E33)),
+            ),
+            Expanded(
+              child: ReusableCard(colour: Color(0xFF1D1E33)),
+            ),
+          ]),
+        ),
+      ]),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({required this.colour});
+
+  Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.all(Radius.circular(10)),
       ),
     );
   }
