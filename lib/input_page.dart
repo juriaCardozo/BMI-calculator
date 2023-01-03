8e20fc8979d7a35398fc89e99dc055f7455f069e
Júlia Cardozo <ccardozojulia@gmail.com>
2023-01-03 15:16:17 -0300

    route Results Page

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,3 +1,4 @@
+import 'package:bmi_calculator_app/results_page.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'icon_content.dart';
@@ -200,11 +201,22 @@ class _InputPageState extends State<InputPage> {
             ),
           ]),
         ),
-        Container(
-          color: kBottomContainerColour,
-          margin: EdgeInsets.only(top: 10),
-          width: double.infinity,
-          height: kBottomContainerHeight,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ResultsPage(),
+              ),
+            );
+          },
+          child: Container(
+            child: Text('CALCULATE'),
+            color: kBottomContainerColour,
+            margin: EdgeInsets.only(top: 10),
+            width: double.infinity,
+            height: kBottomContainerHeight,
+          ),
         ),
       ]),
     );
